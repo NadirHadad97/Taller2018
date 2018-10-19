01b1df1e9203497b45de8e4ba440e3e10c882801
NadirHadad97 <nadir_hadad@hotmail.com>
2018-10-19 18:21:00 -0500

    First commit

---
+++ src/blog/primerblog.md
@@ -0,0 +1,11 @@
+---
+
+tittle: "Hello world"
+date: "2018-09-23"
+author : "Nadir Hadad"
+---
+
+# Este es un h1
+## Ahora un h2
+*Son bold?* 
+_italicas?_
---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({data}) => (
+  <div>
+  
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+
+  <div>
+  {data.allMarkdownRemark.edges.map(({node}) => (
+    <div>
+    <h1>
+    {node.frontmatter.tittle}   </h1>
+    <p>{ node.excerpt}</p>
+    </div>
+  
+  ))}
+
+  </div>
+  </div>
+)
+
+export default IndexPage
+export const query = graphql`
+query HomePageQuery{
+  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+  totalCount
+  edges {
+  node {
+  id
+  fields {
+  slug
+  }
+  frontmatter {
+  title
+  date
+  author
+  }
+  excerpt(pruneLength: 80)
+  timeToRead
+  }
+  }
+  }
+  }
+  `
